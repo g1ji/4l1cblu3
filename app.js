3f850259e95affaf2bce9b61b4d5a8d2f22be53f
g1ji <emails4me.gk@gmail.com>
2021-01-03 01:48:25 +0530

    live

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const cool = require('cool-ascii-faces');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const serveIndex = require('serve-index');
 
 app.use(express.static(__dirname + "/"))
