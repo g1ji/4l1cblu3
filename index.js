3f27420f31ea0bd555b3192b2ee8c61679a804a1
g1ji <emails4me.gk@gmail.com>
2021-01-05 03:31:46 +0530

    ok

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ if (cluster.isMaster) {
     require('./app');
     require('./script');
 }
+// Ly8gaHR0cHM6Ly9nMWppLXdlYmhvb2suaGVyb2t1YXBwLmNvbS8gfCBodHRwczovL2dpdC5oZXJva3UuY29tL2cxamktd2ViaG9vay5naXQKLy8gZ2l0IHB1c2ggaGVyb2t1Ci8vIGhlcm9rdSBsb2dzIC0tdGFpbA==
 // https://g1ji-webhook.herokuapp.com/ | https://git.heroku.com/g1ji-webhook.git
 // git push heroku
 // heroku logs --tail
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const WebSocketClient = require('websocket').client;
 const fs = require('fs');
 const cron = require('node-cron');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 let stocks = {
     3045: "SBIN",
@@ -15,7 +17,8 @@ let stocks = {
     1624: "IOC"
 };
 let query = [];
-let socketUrl = `wss://a${'nt'}.${'ali'}cebl${'ueonl'}${'ine.c'}om/hyd${'rasoc'}ket/${'v2/'}web${'sock'}et`
+let socketUrl = `wss://a${'nt'}.${'ali'}cebl${'ueonl'}${'ine.c'}om/hyd${'rasoc'}ket/${'v2/'}web${'sock'}et`;
+let dbURL = `${'mong'}od${'b+sr'}v://g1${'ji:5ff37a'}efea99c55b4f4d8f5${'5@clu'}ster0.e0${'t4r.mo'}ngo${'db.n'}et/mkt`;
 Object.keys(stocks).forEach((stock) => {
     query.push([1, parseInt(stock)]);
 });
@@ -29,68 +32,82 @@ function bufferToArrayBuffer(buf) {
 }
 
 function connectSocket() {
-    let client = new WebSocketClient();
-    client.on('connectFailed', function (error) {
-        console.log('connectFailed', error)
-        connectSocket();
-    });
-    client.on('connect', function (connection) {
-        connection.on('error', function (error) {
-            client.abort();
-            console.log('connect error', error)
+    mongoose.connect(dbURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    }).then(() => {
+        let collections = {};
+        Object.keys(stocks).forEach((stock) => {
+            query.push([1, parseInt(stock)]);
+            collections[stock] = mongoose.model(`${stock}_${stocks[stock]}`, new Schema({}, { strict: false, collection: `${stock}_${stocks[stock]}` }));
+        });
+
+        let client = new WebSocketClient();
+        client.on('connectFailed', function (error) {
+            console.log('connectFailed', error)
             connectSocket();
         });
-        connection.on('close', function () {
-            console.log('connect close', error)
-            let time = new Date();
-            let currentOffset = time.getTimezoneOffset();
-            let ISTOffset = 330;
-            time = new Date(time.getTime() + (ISTOffset + currentOffset) * 60000);
-            // if (time.getHours() > 8 && time.getHours() < 16) {
-            //     client.abort();
-            //     connectSocket();
-            // }
-        });
-        connection.on('message', function (message) {
-            let time = new Date();
-            let currentOffset = time.getTimezoneOffset();
-            let ISTOffset = 330;
-            time = new Date(time.getTime() + (ISTOffset + currentOffset) * 60000);
-            let dir = `mkt/${time.toISOString().split('T')[0]}`;
-            // if (time.getHours() > 8 && time.getHours() < 16) {
-            let ed = new DataView(bufferToArrayBuffer(message.binaryData));
-            let IT = ed.getUint32(2);
-            if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir);
-            }
-            let path = `${dir}\/${stocks[IT]}-${IT}.txt`
-            fs.appendFile(path, `${time.getTime()}@-->` + message.binaryData.toString('base64') + '\n', function (err) {
-                if (err) {
-                    console.log('File append error', err)
-                }
-            })
-            // } else {
-            //     connection.close();
-            //     client.abort();
-            // }
-
-        });
-
-        function sendHeartBeat() {
-            if (connection.connected) {
-                connection.send(JSON.stringify({ "a": "h", "v": [], "m": "" }));
-            } else {
-                console.log('socket not connected restarting...')
-                connection.close();
+        client.on('connect', function (connection) {
+            connection.on('error', function (error) {
+                client.abort();
+                console.log('connect error', error)
                 connectSocket();
+            });
+            connection.on('close', function () {
+                console.log('connect close', error)
+                let time = new Date();
+                let currentOffset = time.getTimezoneOffset();
+                let ISTOffset = 330;
+                time = new Date(time.getTime() + (ISTOffset + currentOffset) * 60000);
+                if (time.getHours() > 8 && time.getHours() < 16) {
+                    client.abort();
+                    connectSocket();
+                }
+            });
+            connection.on('message', function (message) {
+                let time = new Date();
+                let currentOffset = time.getTimezoneOffset();
+                let ISTOffset = 330;
+                time = new Date(time.getTime() + (ISTOffset + currentOffset) * 60000);
+                let today = `${time.toISOString().split('T')[0]}`;
+                if (time.getHours() > 7 && time.getHours() < 16) {
+                    let ed = new DataView(bufferToArrayBuffer(message.binaryData));
+                    let IT = ed.getUint32(2);
+                    collections[IT].findOneAndUpdate({
+                        "IT": IT,
+                        "SYMBOL": stocks[IT],
+                        "DATE": today
+                    }, {
+                        $set: {
+                            ['data.' + time.getTime()]: message.binaryData.toString('base64')
+                        }
+                    }, { upsert: true }, function (err, doc) {
+                        console.log("err, doc", err, doc)
+                    })
+                } else {
+                    connection.close();
+                    client.abort();
+                }
+            });
+            function sendHeartBeat() {
+                if (connection.connected) {
+                    connection.send(JSON.stringify({ "a": "h", "v": [], "m": "" }));
+                } else {
+                    console.log('socket not connected restarting...')
+                    connection.close();
+                    connectSocket();
+                }
             }
-        }
-        connection.send(JSON.stringify({ "a": "subscribe", "v": query, "m": "marketdata" }));
-        setInterval(sendHeartBeat, 9000)
-    });
-    client.connect(socketUrl);
+            connection.send(JSON.stringify({ "a": "subscribe", "v": query, "m": "marketdata" }));
+            setInterval(sendHeartBeat, 9000)
+        });
+        client.connect(socketUrl);
+    })
 }
-cron.schedule('0 0 9 * * 1,2,3,4,5', () => {
-    console.log("+++++++++++++connecting")
-    connectSocket();
-});
+// cron.schedule('0 30 3 * * 1,2,3,4,5', () => {
+//     console.log("+++++++++++++connecting")
+//     connectSocket();
+// });
+// connectSocket();
